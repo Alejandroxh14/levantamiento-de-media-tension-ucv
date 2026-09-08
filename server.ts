d47d7a897cfcd858349db57a8ff747e98266d16f
ale <alejandroxh14@gmail.com>
2026-09-08 14:41:34 -0400

    cambiar puerto

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 import { createServer as createViteServer } from 'vite';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3007;
 const DATA_DIR = path.join(process.cwd(), 'data');
 const DATA_FILE = path.join(DATA_DIR, 'levantamientos.json');
 
